6b3b8fa5097add561e7748c0f22654e453a04866
littleet9 <littleet@appstate.edu>
2020-11-29 16:55:30 -0500

    Landing page now has signup and register links

--- src/scripts/landingPage.js
+++ src/scripts/landingPage.js
@@ -1,4 +1,36 @@
 function main() {
 	$('#CCFLogoLink').attr('src', 'media/logo.png');
+	initPromptButtons();
 }
 main();
+
+function initPromptButtons(){
+	$('.clickableButton').on('mouseover', function(){
+		$(this).css('cursor', 'pointer');
+		$(this).css('text-shadow', '0px 0px 0px black');
+		$(this).css('box-shadow', '0px 0px 0px black');
+	}).on('mouseout', function(){
+		$(this).css('cursor', 'default');
+		$(this).css('text-shadow', '2px 2px 2px black');
+		$(this).css('box-shadow', '3px 3px 2px black');
+	});
+
+	//Sign-In
+	$('#SignInButton').on('click', function(){
+		$('#prompt').css('display', 'none');
+		$('#register').css('display', 'none');
+		$('#signIn').css('display', 'inline');
+	});
+	//Register
+	$('#RegisterButton').on('click', function(){
+		$('#prompt').css('display', 'none');
+		$('#register').css('display', 'inline');
+		$('#signIn').css('display', 'none');
+	});
+	//GoBack
+	$('.backButton').on('click', function(){
+		$('#prompt').css('display', 'inline');
+		$('#register').css('display', 'none');
+		$('#signIn').css('display', 'none');
+	});
+}
