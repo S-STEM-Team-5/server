4adbd252bd7480399c1b61b70b7cedc7a42b9c38
littleet9 <littleet@appstate.edu>
2020-11-30 11:45:17 -0500

    Some Updates for Volunteer Dashboard

--- src/scripts/volunteerDashboard.js
+++ src/scripts/volunteerDashboard.js
@@ -1,10 +1,20 @@
 function main() {
-	initPromptButtons();
+	initWelcome();
+	initApplicationButtons();
+	initStatus();
 }
 main();
 
-function initPromptButtons(){
-	$('.clickableButton').on('mouseover', function(){
+function initWelcome(){
+	//TODO: Get Name
+
+	let dataName = "Ethan";
+
+	$('#welcome').text("Welcome " + dataName +"!");
+}
+
+function initApplicationButtons(){
+	$('button').on('mouseover', function(){
 		$(this).css('cursor', 'pointer');
 		$(this).css('background-color', 'rgb(73, 73, 73)');
 	}).on('mouseout', function(){
@@ -13,10 +23,8 @@ function initPromptButtons(){
 	});
 
 	//Sign-In
-	$('#SignInButton').on('click', function(){
-		$('#prompt').css('display', 'none');
-		$('#register').css('display', 'none');
-		$('#signIn').css('display', 'inline');
+	$('#newApplicationButton').on('click', function(){
+		window.location.href = "volApplication1.html";
 	});
 	//Register
 	$('#RegisterButton').on('click', function(){
@@ -30,4 +38,25 @@ function initPromptButtons(){
 		$('#register').css('display', 'none');
 		$('#signIn').css('display', 'none');
 	});
+}
+
+function initStatus(){
+	let Status = $('#approvalStatus');
+	let Weeks = $('#weeksToWork');
+	//TODO: Get Status from Database
+	
+	let dataStatus = "Not Submitted";	//Options: Approved, Pending, Declined, and Not Submitted
+	
+	//Update the status
+	Status.text("Status: " + dataStatus);
+	if(dataStatus == "Approved"){
+		//TODO: Get Weeks from Database
+
+		let dataWeeks = "Kids, Siblings";
+
+		Weeks.text("Weeks: " + dataWeeks);
+	}
+	else {
+		Weeks.text("Weeks: Unavailable")
+	}
 }
