9b8ced72277e5c27bd95e1fbb1bf7a089de76b18
littleet9 <littleet@appstate.edu>
2020-12-01 15:10:35 -0500

    Connecting Register to API

--- src/scripts/landingPage.js
+++ src/scripts/landingPage.js
@@ -1,70 +1,125 @@
 function main() {
-  $('#CCFLogoLink').attr('src', 'media/logo.png');
-  initPromptButtons();
+	$('#CCFLogoLink').attr('src', 'media/logo.png');
+	initPromptButtons();
+	initSubmitButtons();
 }
 main();
-
+/* ----------------------------------------------------
+	initPromptButtons
+   ----------------------------------------------------*/
 function initPromptButtons() {
-  $('.clickableButton').on('mouseover', function() {
-    $(this).css('cursor', 'pointer');
-    $(this).css('background-color', 'rgb(73, 73, 73)');
-  }).on('mouseout', function() {
-    $(this).css('cursor', 'default');
-    $(this).css('background-color', 'rgb(185, 185, 185)');
-  });
+	$('.clickableButton').on('mouseover', function() {
+		$(this).css('cursor', 'pointer');
+		$(this).css('background-color', 'rgb(73, 73, 73)');
+	}).on('mouseout', function() {
+		$(this).css('cursor', 'default');
+		$(this).css('background-color', 'rgb(185, 185, 185)');
+	});
 
-  //Sign-In
-  $('#SignInButton').on('click', function() {
-    $('#prompt').css('display', 'none');
-    $('#register').css('display', 'none');
-    $('#signIn').css('display', 'inline');
-  });
-  //Register
-  $('#RegisterButton').on('click', function() {
-    $('#prompt').css('display', 'none');
-    $('#register').css('display', 'inline');
-    $('#signIn').css('display', 'none');
-  });
-  //GoBack
-  $('.backButton').on('click', function() {
-    $('#prompt').css('display', 'inline');
-    $('#register').css('display', 'none');
-    $('#signIn').css('display', 'none');
-  });
-
-  $('#submitButton').on('click', function() {
-    if ($('#sEmail').val() != "" && ($('#sPassword').val() != "")) {
-      let jsonEmailTemp = {
-        email: $('#sEmail').val(),
-        password: $('#sPassword').val()
-      }
-      const response = postRequest().then((response) => {
+	//Sign-In
+	$('#SignInButton').on('click', function() {
+		$('#prompt').css('display', 'none');
+		$('#register').css('display', 'none');
+		$('#signIn').css('display', 'inline');
+	});
+	//Register
+	$('#RegisterButton').on('click', function() {
+		$('#prompt').css('display', 'none');
+		$('#register').css('display', 'inline');
+		$('#signIn').css('display', 'none');
+	});
+	//GoBack
+	$('.backButton').on('click', function() {
+		$('#prompt').css('display', 'inline');
+		$('#register').css('display', 'none');
+		$('#signIn').css('display', 'none');
+		resetErrorMessages();
+	});
+}
+function resetErrorMessages(){
+	$('#rErrorMessage, #sErrorMessage').css('display','none').text("");
+}
+/* ----------------------------------------------------
+	initSubmitButtons and SignIn and Register Forms 
+   ----------------------------------------------------*/
+function initSubmitButtons() {
+	//Sign In
+	$('#sSubmitButton').on('click', function() {
+		if ($('#sEmail').val() != "" && ($('#sPassword').val() != "")) {
+			const response = postRequestSignIn().then((response) => {
 				if (response.message == "Cannot find Account") {
-	        $('#sErrorMe').text("Invaild Credentails");
-					$('#sErrorMe').css('display', 'inline');
-	      } else {
-	        $('#sErrorMe').text("");
-	      }
+					$('#sErrorMessage').text("Invaild Credentails");
+					$('#sErrorMessage').css('display', 'inline');
+				} else {
+					$('#sErrorMessage').text("");
+					$('#sErrorMessage').css('display', 'none');
+				}
 			});
-    }
-  });
-
-
-  const getRequest = async () => {
-    try {
-      const response = await axios.get("http://localhost:3013/rest/account/");
-      console.log(response.data[0].email);
-    } catch (err) {
-      console.log(err);
-    }
-  }
+		}
+	});
+	$('#signInForm, #registerForm').submit(function (e) {
+		e.preventDefault();
+	});
+	//Register
+	$('#rSubmitButton').on('click', function(){
+		if($('#fName').val() != "" &&
+		   $('#lName').val() != "" &&
+		   $('#rEmail').val() != "" &&
+		   $('#rPassword').val() != "" &&
+		   $('#rRPassword').val() != "")
+		{
+			if($('#rPassword').val() != $('#rRPassword').val()){
+				$('#rErrorMessage').text("Passwords do not match");
+				$('#rErrorMessage').css('display', 'inline');
+			}else{
+				$('#rErrorMessage').text("");
+				$('#rErrorMessage').css('display', 'none');
+				const response = postRequestRegister().then((response) => {
+					if (response.message == "Cannot find Account") {
+						$('#sErrorMessage').text("Invaild Credentails");
+						$('#sErrorMessage').css('display', 'inline').css('color', 'red');
+					} else {
+						$('#sErrorMessage').text("Account Created");
+						$('#sErrorMessage').css('display', 'inline').css('color', 'green');
+					}
+				});
+			}
+		}
+	});
 }
 
- async function postRequest() {
+async function getRequest() {
+    try {
+		const response = await axios.get("http://localhost:3013/rest/account/");
+		console.log(response.data[0].email);
+	  } catch (err) {
+		console.log(err);
+	  }
+}
+//Sign In Post Request
+async function postRequestSignIn() {
   var data = {
 		"email": $('#sEmail').val(),
 		"password": $('#sPassword').val()
   }
   const response = await axios.post('http://localhost:3013/rest/account/signin', data);
+	return response.data;
+}
+//Register Post Request
+async function postRequestRegister() {
+	let accTypeS = "Parent";
+	if($('radioVolunteer').attr('checked') == true)
+		accTypeS = "Volunteer";
+	var data = {
+		"accType": accTypeS,
+		"email": $('#rEmail').val(),
+		"password": $('#rPassword').val(),
+		"name" : {
+			"fname" : $('#fName').val(),
+			"lname" : $('#lName').val()
+		}
+	}
+	const response = await axios.post('http://localhost:3013/rest/account', data);
+	console.log(response);
 	return response.data;
 }
